ce2d1e27098cf7c12dbe2f98891a63fffc33527f
Hound672 <hound672@gmail.com>
2020-01-04 22:48:29 +0300

    Add endpoint for test model.

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get('/')
 def root():
     return {'root': 'root'}
+
+
+class ModelNested(BaseModel):
+    val_nested1: int
+    val_nested2: str
+
+class ModelMain(BaseModel):
+    val_main: int
+    val_nested: ModelNested
+
+
+@app.post('/model')
+def model(item: ModelMain):
+    return {'model': 'model'}
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
